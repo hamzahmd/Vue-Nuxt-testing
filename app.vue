90c7e3b98a694e4a3640333116661c8fdf9d4aa9
Hamza Ahmad <hamzahmd.dev@gmail.com>
2023-03-07 11:38:54 +0500

    Formkit vue/nuxt

---
+++ app.vue
@@ -0,0 +1,18 @@
+<!-- <template>
+  <div v-if="error" style="color: red">Error :{{ error }}</div>
+  <div v-else-if="isFetching" style="color: blue">loading...</div>
+  <div v-else="error" style="color: green">{{ data }}</div>
+  <input type="number" v-model="id" />
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { useFetch } from '@vueuse/core';
+const id = ref(1);
+const url = computed(() => {
+  return `https://jsonplaceholder.typicode.com/todos/${id.value}`;
+});
+const { isFetching, data, error } = useFetch(url, {
+  refetch: true,
+});
+</script> -->
